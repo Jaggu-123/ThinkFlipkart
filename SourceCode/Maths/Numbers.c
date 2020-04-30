a31516d9d93dbe6133210aac76d3c9b74c143348
Jaggu-123 <iit2017503@iiita.ac.in>
2020-04-30 12:21:22 +0530

    Added Contents in 2 File: 	Added File Maths/Number.c and added code 	Added File String/Greeting.cpp and added a message

---
+++ SourceCode/Maths/Numbers.c
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+int main(){
+	
+	int t;
+	cin>> t;
+
+	cout<<"Number is "<<t<<endl;
+	return 0
+}
---
+++ SourceCode/String/Greeting.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	cout<<"Hello World!"<<endl;
+	cout<<"Be Safe"<<endl;
+	return 0;
+}
