93bfc8175775aa4b7008a0968e5fd86a5e2223bb
Jaggu-123 <iit2017503@iiita.ac.in>
2020-04-30 11:43:19 +0530

    Added Functionality For Greeting: 	Added Few Messages Udated Hello.c

--- SourceCode/Hello.c
+++ SourceCode/Hello.c
@@ -3,7 +3,8 @@
 int main(){
 
 	printf("Hello World");
-
+	printf("Ding Dong");
+	printf("Modifying File");
 	return 0;
 
 }
---
+++ SourceCode/String.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+void greeting(char Hello[]){
+	printf("%s\n", Hello);
+}
+
+int main(){
+
+	char Hello[] = "Hello Sir!";
+	greeting(Hello);
+
+	return 0;
+
+}
